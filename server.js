fe991829b6b06a175ca0d1ce877bf5606ee217b1
MaximeBicc <maxime.bicchierai@email.com>
2026-03-09 21:09:42 +0100

    Initial backend

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Home Trainer API running" });
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await db.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
