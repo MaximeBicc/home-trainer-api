9c238b06fe6ced6299ec9cf29649fb663aa1aa29
MaximeBicc <maxime.bicchierai@gmail.com>
2026-03-09 21:41:51 +0100

    Update log message for DATABASE_URL existence

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
 
 const express = require("express");
 const cors = require("cors");
